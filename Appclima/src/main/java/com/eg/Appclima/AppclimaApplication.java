e18231e8918f64ccf2ba48c21f28a57ea58f8483
ErickAlas07 <91312014+ErickAlas07@users.noreply.github.com>
2024-02-08 22:54:57 -0600

    Creación de proyecto y colocación de dependencias

---
+++ Appclima/src/main/java/com/eg/Appclima/AppclimaApplication.java
@@ -0,0 +1,13 @@
+package com.eg.Appclima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppclimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppclimaApplication.class, args);
+	}
+
+}
